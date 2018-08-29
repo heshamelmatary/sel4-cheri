f96ad9979c4ee52d7bbf4fdae49c3ba1a58b4fb3
Hesham Almatary <Hesham.Almatary@cl.cam.ac.uk>
2018-08-29 14:57:44 +0100

    CHERI: Properly guard CHERI-related code

--- src/arch/riscv/kernel/boot.c
+++ src/arch/riscv/kernel/boot.c
@@ -394,19 +394,16 @@ try_init_kernel(
     /* finalise the bootinfo frame */
     bi_finalise();
 
-#ifdef CONFIG_DEBUG_BUILD
+#ifdef CONFIG_ARCH_CHERI
     for (int i = 0; i <= ndks_boot.slot_pos_cur; i++) {
-        seL4_Cap cap = *((seL4_Cap *) SLOT_PTR(cap_cnode_cap_get_capCNodePtr(root_cnode_cap) , i));
+        //seL4_Cap cap = *((seL4_Cap *) SLOT_PTR(cap_cnode_cap_get_capCNodePtr(root_cnode_cap) , i));
         printf("#%d: 0x%lx%lx\n", i, cap.words[1], cap.words[0]);
 
         /* Copy all root cnoide caps to user's bootinfo */
         ndks_boot.bi_frame->user_cnode_root[i] = cap;
 
-#ifdef CONFIG_ARCH_CHERI
         /* Tag it */
         asm volatile("ctag %0" :: "r" (&(ndks_boot.bi_frame->user_cnode_root[i])));
-#endif
-
     }
 #endif
 
